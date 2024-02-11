0918faa3c49b080f9c5b034ec88dc2e5dcd52f09
TAPASYA BAIDYA <83876783+knownstranger-Tapasya@users.noreply.github.com>
2024-02-11 18:22:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const eduContainers = document.querySelectorAll('.educontainer');
+
+eduContainers.forEach((container, index) => {
+    container.classList.add('slide-in');
+    container.style.animationDelay = `${index * 2}s`;
+});
+
+
+
+var menuToggle = document.getElementById('checkbtn');
+var list = document.querySelector('ul');
+var closeIcon = document.querySelector('.closebtn');
+
+menuToggle.addEventListener('change', function() {
+    list.style.left = '0';
+    closeIcon.style.display = 'block';
+    menuToggle.style.display = 'none';
+
+});
+
+closeIcon.addEventListener('click', function() {
+    menuToggle.checked = false;
+    list.style.left = '-100%';
+    closeIcon.style.display = 'none';
+    menuToggle.style.display = 'block';
+});
+
+var listItems = document.querySelector('ul li a');
